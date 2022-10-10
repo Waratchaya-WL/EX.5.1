88bd38107d2c333a0ecd1fab1f4ffa7d7bd91ad0
Waratchaya-WL <dada17580@gmail.com>
2022-10-10 21:21:25 +0700

    5 commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.cyan,
       ),
       home: const MyHomePage(title: 'แอพบัญชี'),
@@ -25,7 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  
   final String title;
 
   @override
@@ -33,25 +30,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton
-          (icon: Icon(Icons.add), 
-          onPressed: (){
-          Navigator .push(context, MaterialPageRoute(builder: (context){
-            return FormScreen();
-          }));
-          })
-        ],
-      ),
-      body: Container()
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return FormScreen();
+                  }));
+                })
+          ],
+        ),
+        body: ListView.builder(itemBuilder: (context, int index) {
+          return Card(
+            elevation: 5,
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: FittedBox(
+                  child: Text("500"),
+                ),
+              ),
+              title: Text("รายการ"),
+              subtitle: Text("10/10/2022"),
+            ),
+          );
+        }));
   }
 }
