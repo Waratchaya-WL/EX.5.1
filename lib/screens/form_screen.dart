a6733af96ff76400a1a8509d29bb7e118b8ded5b
Waratchaya-WL <dada17580@gmail.com>
2022-10-10 19:52:23 +0700

    4 commit

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -10,7 +10,26 @@ class FormScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("แบบฟอร์มบันทึกข้อมูล"),
       ),
-      body: Container()
-    );
+      body: Form(
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            decoration: new InputDecoration(labelText: "ชื่อรายการ"),
+            autofocus: true,
+          ),
+          TextFormField(
+            decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+            keyboardType: TextInputType.number,
+          ),
+          FloatingActionButton(
+            child: Text("เพิ่มข้อมูล"),
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            )
+        ],
+        ),
+        ));
   }
 }
