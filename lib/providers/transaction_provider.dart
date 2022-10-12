0315f6e25cb9a235646d2025e1fd74f5e010ccda
Waratchaya-WL <dada17580@gmail.com>
2022-10-12 14:46:30 +0700

    6 commit

---
+++ lib/providers/transaction_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_application_1/models/Transaction.dart';
+
+class TransactionProvider with ChangeNotifier{
+   List<Transaction> transactions = [
+    Transaction(title: "ซื่้อหนังสือ",amount: 500,date: DateTime.now()),
+    Transaction(title: "เสื้อผ้า",amount: 900,date: DateTime.now()),
+    Transaction(title: "กางเกง",amount: 400,date: DateTime.now()),
+   ];
+
+   List<Transaction>getTransaction(){
+    return transactions;
+   }
+   void addTransaction(Transaction statement){
+      transactions.add(statement);
+   }
+}
