c38bf32d2821e1b6d4442e9f4152224e3770ce75
Waratchaya-WL <dada17580@gmail.com>
2022-10-14 15:59:19 +0700

    9 commit

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
   const FormScreen({super.key});
-
+  final formKey = GlobalKey<FormState>();
   @override
  Widget build(BuildContext context) {
    
@@ -11,21 +11,39 @@ class FormScreen extends StatelessWidget {
         title: Text("แบบฟอร์มบันทึกข้อมูล"),
       ),
       body: Form(
+        key: formKey,
         child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           TextFormField(
             decoration: new InputDecoration(labelText: "ชื่อรายการ"),
             autofocus: true,
+            validator: (String str){
+                if(str.isEmpty){
+                  return"กรุณาป้อนชื่อรายการ";
+                }
+                return null;
+            },
           ),
           TextFormField(
             decoration: new InputDecoration(labelText: "จำนวนเงิน"),
             keyboardType: TextInputType.number,
+            validator: (String str){
+              if(str.isEmpty){
+                return "กรุณาป้อนจำนวนเงิน";
+              }
+              if(double.parse(str)<=0){
+                    return "กรุณาป้อนตัวเลขมากกว่า 0";
+              }
+              return null;
+            },
           ),
           FloatingActionButton(
             child: Text("เพิ่มข้อมูล"),
             onPressed: (){
+              if(formKey.currentState.validate()){
               Navigator.pop(context);
+              }
             },
             )
         ],
