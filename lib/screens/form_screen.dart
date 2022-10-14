7e9824b8d439df82363c2546583f1b48647a36df
Waratchaya-WL <dada17580@gmail.com>
2022-10-14 16:10:33 +0700

    10 commit

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class FormScreen extends StatelessWidget {
   const FormScreen({super.key});
   final formKey = GlobalKey<FormState>();
+
+  final titlController = TextEditingController();
+  final amountController = TextEditingController();
   @override
  Widget build(BuildContext context) {
    
@@ -18,6 +21,7 @@ class FormScreen extends StatelessWidget {
           TextFormField(
             decoration: new InputDecoration(labelText: "ชื่อรายการ"),
             autofocus: true,
+            controller: titlController,
             validator: (String str){
                 if(str.isEmpty){
                   return"กรุณาป้อนชื่อรายการ";
@@ -28,6 +32,7 @@ class FormScreen extends StatelessWidget {
           TextFormField(
             decoration: new InputDecoration(labelText: "จำนวนเงิน"),
             keyboardType: TextInputType.number,
+            controller: amountController,
             validator: (String str){
               if(str.isEmpty){
                 return "กรุณาป้อนจำนวนเงิน";
@@ -42,6 +47,11 @@ class FormScreen extends StatelessWidget {
             child: Text("เพิ่มข้อมูล"),
             onPressed: (){
               if(formKey.currentState.validate()){
+                var title = titlController.text;
+                var amount = amountController.text;
+
+                print(title);
+                print(amount);
               Navigator.pop(context);
               }
             },
