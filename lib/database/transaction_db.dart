a9bb6c4a61aeb9dd86450a056c852301a662cf0f
Waratchaya-WL <dada17580@gmail.com>
2022-10-14 19:18:06 +0700

    16 commit

--- lib/database/transaction_db.dart
+++ lib/database/transaction_db.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_application_1/models/Transactions.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
@@ -18,5 +19,13 @@ class TransactionDB{
          Database db = await dbFactory.openDatabase(dbLocation);
          return db;
      }
-
+       InsertData(Transactions statement) async{
+       var db = await this.openDatabase();
+       var store = intMapStoreFactory.store("expense");
+       store.add(db, {
+        "title":statement.title,
+        "amount":statement.amount,
+        "date":statement.date,
+       });
+       }
 }
--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -3,14 +3,14 @@ import 'package:flutter_application_1/database/transaction_db.dart';
 import 'package:flutter_application_1/models/Transactions.dart';
 
 class TransactionProvider with ChangeNotifier{
-   List<Transaction> transactions = [];
+   List<Transactions> transactions = [];
 
    List<Transactions>getTransaction(){
     return transactions;
    }
    void addTransaction(Transactions statement) async{
-      var db=await TransactionDB(dbName:"transactions.db").openDatabase();
-      print(db);
+      var db=await TransactionDB(dbName:"transactions.db");
+      await db.InsertData(statement);
       transactions.insert(0,statement);
       notifyListeners();
    }
